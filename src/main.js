a1bcc75dae57ebb01789b0c5ac834be85c84cf7a
Oksana Piddubniak <oksana.piddubniak.v@gmail.com>
2026-09-16 23:39:52 +0300

    Header was added

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import "./style.css";
+
+const headerNav = document.querySelector(".header-nav");
+const menuOpen = document.querySelector(".menu-open");
+
+menuOpen.addEventListener("click", () => {
+  menuOpen.classList.toggle("active-menu");
+  headerNav.classList.toggle("active");
+});
